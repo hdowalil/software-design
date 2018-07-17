8194cc4a432293338d2abacb0d6787de2a4f2915
Herbert <hdowalil@gmail.com>
2018-07-17 19:24:37 +0200

    dependency from c1 to c5

--- training-design/design-dependency-inversion/src/main/java/lab02/sample/solution/a/C1.java
+++ training-design/design-dependency-inversion/src/main/java/lab02/sample/solution/a/C1.java
@@ -2,7 +2,7 @@ package lab02.sample.solution.a;
 
 import javax.inject.Inject;
 
-import lab02.sample.solution.b.C4;
+import lab02.sample.solution.b.C5;
 
 public class C1 {
 	
@@ -11,14 +11,14 @@ public class C1 {
 	@Inject
 	C3 c3;
 	@Inject
-	C4 c4;
+	C5 c5;
 	
 	public void do1() {
 		
 		System.out.println("Solution-1");
 		c2.do2();
 		c3.do3();
-		c4.do4();
+		c5.do5();
 	}
 
 }
